a1b3d0576ecaacc957a02b1cf95e5c5f4d54f6ac
evan <evan.vallet@uha.fr>
2025-02-27 11:45:27 +0100

    push terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,3 +14,35 @@ provider "proxmox" {
   password = "totototo"
 
 }
+
+resource "proxmox_virtual_environment_vm" "node_clone" {
+  name      = "node-clone"
+  node_name = "evan"
+
+  clone {
+    vm_id = linux-template
+  }
+
+  agent {
+    enabled = true
+  }
+
+  memory {
+    dedicated = 2048
+  }
+
+  initialization {
+    dns {
+      servers = ["8.8.8.8"]
+    }
+    ip_config {
+      ipv4 {
+        address = "10.129.4.165"
+      }
+    }
+  }
+}
+
+output "vm_ipv4_address" {
+  value = proxmox_virtual_environment_vm.node_clone.ipv4_addresses[1][0]
+}
--- terraform/variables.tf
+++
@@ -1,4 +0,0 @@
-provider "proxmox" {
-  endpoint  = "http://10.129.4.160:8006/"
-  api_token = "terraform@pve!provider=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-}
